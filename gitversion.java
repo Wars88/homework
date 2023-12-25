08a024d82aa0a649d9a573dc87b3804db847f61b
Wars88 <dnjswns808@gmail.com>
2023-12-25 14:53:05 +0900

    버전 1.2

---
+++ gitversion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javawork;
+
+/**
+ *
+ * @author LG
+ */
+public class gitversion {
+    // 버전추가
+}
